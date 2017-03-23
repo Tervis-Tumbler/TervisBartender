d5418dc764f54981ce09bac1ee99dd74fd269396
Chris Magnuson <hourglasssand@gmail.com>
2017-03-23 15:23:37 -0400

    Remove depricated -excludevm

--- TervisBartender.psm1
+++ TervisBartender.psm1
@@ -1,11 +1,9 @@
 ﻿function Get-BartenderCommanderNodes {
-    Get-TervisClusterApplicationNode -ClusterApplicationName BartenderCommander -ExludeVM
+    Get-TervisClusterApplicationNode -ClusterApplicationName BartenderCommander
 }
 
 function Invoke-BartenderCommanderProvision {
     Invoke-ClusterApplicationProvision -ClusterApplicationName BartenderCommander
     $Nodes = Get-BartenderCommanderNodes
     $Nodes | Install-WCSPrintersForBartenderCommander
-
-
 }
