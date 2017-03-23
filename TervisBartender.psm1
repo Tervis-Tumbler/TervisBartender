e95e3cc23faf527bc3f7a648b9e38a3100607a4e
Chris Magnuson <hourglasssand@gmail.com>
2017-03-23 11:43:11 -0400

    Initial commit

---
+++ TervisBartender.psm1
@@ -0,0 +1,11 @@
+﻿function Get-BartenderCommanderNodes {
+    Get-TervisClusterApplicationNode -ClusterApplicationName BartenderCommander -ExludeVM
+}
+
+function Invoke-BartenderCommanderProvision {
+    Invoke-ClusterApplicationProvision -ClusterApplicationName BartenderCommander
+    $Nodes = Get-BartenderCommanderNodes
+    $Nodes | Install-WCSPrintersForBartenderCommander
+
+
+}
