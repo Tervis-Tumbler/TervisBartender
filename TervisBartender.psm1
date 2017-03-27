a75fb574455af770fecbb168347106f4d630ced5
Chris Magnuson <hourglasssand@gmail.com>
2017-03-27 15:22:31 -0400

    Add creation of ODBC connections

--- TervisBartender.psm1
+++ TervisBartender.psm1
@@ -6,4 +6,6 @@ function Invoke-BartenderCommanderProvision {
     Invoke-ClusterApplicationProvision -ClusterApplicationName BartenderCommander
     $Nodes = Get-BartenderCommanderNodes
     $Nodes | Install-WCSPrintersForBartenderCommander
+    $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName Tervis
+    $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName tervisBartender
 }
