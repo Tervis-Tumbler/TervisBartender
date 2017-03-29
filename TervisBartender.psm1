1c7bcd2277dd0db5bad066afec0298927aaf10d3
Chris Magnuson <hourglasssand@gmail.com>
2017-03-29 16:04:21 -0400

    Add Set-BTLMIniFile

--- TervisBartender.psm1
+++ TervisBartender.psm1
@@ -8,4 +8,19 @@ function Invoke-BartenderCommanderProvision {
     $Nodes | Install-WCSPrintersForBartenderCommander
     $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName Tervis
     $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName tervisBartender
+    $Nodes | Set-BTLMIniFile
+}
+
+function Set-BTLMIniFile {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    begin {
+        $BartenderInstallPath = "C:\Program Files (x86)\Seagull\BarTender Suite"    
+        $ModulePath = (Get-Module -ListAvailable TervisBartender).ModuleBase
+    }
+    process {
+        $BartenderInstallPathOnNode = "\\$ComputerName\$($BartenderInstallPath -replace ":","$")"
+        Copy-Item -Path $ModulePath\BTLM.ini -Destination $BartenderInstallPathOnNode\BTLM.ini -Force
+    }
 }
